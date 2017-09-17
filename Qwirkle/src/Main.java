5eb25165268b0356d89736080d899e800017d084
KarimAK99 <KarimAbdelKader@outlook.com>
2017-09-17 16:31:47 +0200

    tiles added

--- Qwirkle/src/Main.java
+++ Qwirkle/src/Main.java
@@ -1,19 +1,57 @@
+import java.awt.List;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		
-		
-
 	}
 	
+	public static ArrayList<Tile> bag = new ArrayList<Tile>();
 	
-	public void generateTiles(){
+	public void generateBag(){
 		
-		
-		
-		
+		for (int i = 0; i < 3; i++){
+			
+			bag.add(Tile.tile1);
+			bag.add(Tile.tile2);
+			bag.add(Tile.tile3);
+			bag.add(Tile.tile4);
+			bag.add(Tile.tile5);
+			bag.add(Tile.tile6);
+			bag.add(Tile.tile7);
+			bag.add(Tile.tile8);
+			bag.add(Tile.tile9);
+			bag.add(Tile.tile10);
+			bag.add(Tile.tile11);
+			bag.add(Tile.tile12);
+			bag.add(Tile.tile13);
+			bag.add(Tile.tile14);
+			bag.add(Tile.tile15);
+			bag.add(Tile.tile16);
+			bag.add(Tile.tile17);
+			bag.add(Tile.tile18);
+			bag.add(Tile.tile19);
+			bag.add(Tile.tile20);
+			bag.add(Tile.tile21);
+			bag.add(Tile.tile22);
+			bag.add(Tile.tile23);
+			bag.add(Tile.tile24);
+			bag.add(Tile.tile25);
+			bag.add(Tile.tile26);
+			bag.add(Tile.tile27);
+			bag.add(Tile.tile28);
+			bag.add(Tile.tile29);
+			bag.add(Tile.tile30);
+			bag.add(Tile.tile31);
+			bag.add(Tile.tile32);
+			bag.add(Tile.tile33);
+			bag.add(Tile.tile34);
+			bag.add(Tile.tile35);
+			bag.add(Tile.tile36);
+			
+		}				
 	}
 
 }
--- Qwirkle/src/Tile.java
+++ Qwirkle/src/Tile.java
@@ -25,6 +25,57 @@ public class Tile {
 		this.shape = shape;
 	}
 	
-	
+		
+		public final static Colour red = new Colour(1, "red");
+		public final static Colour orange = new Colour(2, "orange");
+		public final static Colour yellow = new Colour(3, "yellow");
+		public final static Colour green = new Colour(4, "green");
+		public final static Colour blue = new Colour(5, "blue");
+		public final static Colour purple = new Colour(6, "purple");
+		
+		public final static Shape circle = new Shape(1, "circle");
+		public final static Shape x = new Shape(2, "x");
+		public final static Shape diamond = new Shape(3, "diamond");
+		public final static Shape square = new Shape(4, "square");
+		public final static Shape star = new Shape(5, "star");
+		public final static Shape cross = new Shape(6, "cross");		
+		
+		public final static Tile tile1 = new Tile(red, circle);
+		public final static Tile tile2 = new Tile(red, x);
+		public final static Tile tile3 = new Tile(red, diamond);
+		public final static Tile tile4 = new Tile(red, square);
+		public final static Tile tile5 = new Tile(red, star);
+		public final static Tile tile6 = new Tile(red, cross);
+		public final static Tile tile7 = new Tile(orange, circle);
+		public final static Tile tile8 = new Tile(orange, x);
+		public final static Tile tile9 = new Tile(orange, diamond);
+		public final static Tile tile10 = new Tile(orange, square);
+		public final static Tile tile11 = new Tile(orange, star);
+		public final static Tile tile12 = new Tile(orange, cross);
+		public final static Tile tile13 = new Tile(yellow, circle);
+		public final static Tile tile14 = new Tile(yellow, x);
+		public final static Tile tile15 = new Tile(yellow, diamond);
+		public final static Tile tile16 = new Tile(yellow, square);
+		public final static Tile tile17 = new Tile(yellow, star);
+		public final static Tile tile18 = new Tile(yellow, cross);
+		public final static Tile tile19 = new Tile(green, circle);
+		public final static Tile tile20 = new Tile(green, x);
+		public final static Tile tile21 = new Tile(green, diamond);
+		public final static Tile tile22 = new Tile(green, square);
+		public final static Tile tile23 = new Tile(green, star);
+		public final static Tile tile24 = new Tile(green, cross);
+		public final static Tile tile25 = new Tile(blue, circle);
+		public final static Tile tile26 = new Tile(blue, x);
+		public final static Tile tile27 = new Tile(blue, diamond);
+		public final static Tile tile28 = new Tile(blue, square);
+		public final static Tile tile29 = new Tile(blue, star);
+		public final static Tile tile30 = new Tile(blue, cross);
+		public final static Tile tile31 = new Tile(purple, circle);
+		public final static Tile tile32 = new Tile(purple, x);
+		public final static Tile tile33 = new Tile(purple, diamond);
+		public final static Tile tile34 = new Tile(purple, square);
+		public final static Tile tile35 = new Tile(purple, star);
+		public final static Tile tile36 = new Tile(purple, cross);
+			
 	
 }
